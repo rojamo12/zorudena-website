a139e1e70eb70efe506cd8086b3850d379ad28e6
rojamo12 <omirambe223@gmail.com>
2025-07-08 00:18:08 +0200

    Fix ALLOWED_HOSTS for Railway domain

--- zorudena/settings.py
+++ zorudena/settings.py
@@ -17,8 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['localhost','127.0.0.1','zorudena.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://zorudena.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','zorudena-website-production-fbf7.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://zorudena-website-production-fbf7.up.railway.app']
 
 # Application definition
 
